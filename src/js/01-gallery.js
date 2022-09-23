35637a3a1a0025cbcaf76f79dbe0772cb6a061b2
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-09-23 21:48:35 +0300

    Додай бібліотеку SimpleLightbox як залежність проекту

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,40 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gall = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items.map((item) => `<div class="gallery__item"> 
+  <a class="gallery__link" href="${item.original}"> 
+  <img class="gallery__image" 
+  src="${item.preview}" 
+  data-source="${item.original}" 
+  alt="${item.description}" /> 
+  </a> 
+  </div>`).join('')
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+gall.innerHTML = addGalleryMarkup;
+gall.addEventListener("click", onImageClick);
+function onImageClick(evt) {
+  // заборона відкривання картинки, браузером
+  blockStandartAction(evt);
+
+  if (evt.target.nodeName !== 'IMG'){
+    return
+  }
+  
+ var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+  gall.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+        lightbox.close()     
+    }
+  })
+}
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
